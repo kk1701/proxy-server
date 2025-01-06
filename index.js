a73aededdede204268f4ec6cdcc9fec9bf7b5e70
kk1701 <karankute05@gmail.com>
2025-01-06 13:17:16 +0530

    handled preflight requests

--- index.js
+++ index.js
@@ -7,11 +7,19 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header["Access-Control-Allow-Origin"] = "*";
-  res.header["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS";
-  res.header["Access-Control-Allow-Headers"] = "Content-Type, Authorization";
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 });
+
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.send();
+});
+
 
 app.use(
   "/proxy",
