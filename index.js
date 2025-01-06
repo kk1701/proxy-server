769f102fc773f61bd515667fb34953ed00c8386e
kk1701 <karankute05@gmail.com>
2025-01-06 12:31:26 +0530

    updated cors origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
       proxyReq.setHeader("Origin", "https://api.langflow.astra.datastax.com");
     },
     onProxyRes: (proxyRes, req, res) => {
-      proxyRes.headers["Access-Control-Allow-Origin"] = "*"; // React app's URL
+      proxyRes.headers["Access-Control-Allow-Origin"] = "http://localhost:5173/"; // React app's URL
       proxyRes.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS";
       proxyRes.headers["Access-Control-Allow-Headers"] =
         "Content-Type, Authorization";
